8140c6c26b6957d9a024d93808320b2a43116a08
surinder2000 <surinder20002017@gmail.com>
2020-07-12 01:14:12 +0530

    first commit

---
+++ app.sh
@@ -0,0 +1,7 @@
+
+kubectl create -f mysqlpvc.yml
+kubectl create -f mysqldeploy.yml
+kubectl create -f service.yml
+kubectl create -f wppvc.yml
+kubectl create -f wordpressdeploy.yml
+kubectl create -f wpservice.yml
---
+++ delapp.sh
@@ -0,0 +1,6 @@
+kubectl delete -f mysqldeploy.yml
+kubectl delete -f service.yml
+kubectl delete -f wordpressdeploy.yml
+kubectl delete -f wpservice.yml
+kubectl delete -f mysqlpvc.yml
+kubectl delete -f wppvc.yml
